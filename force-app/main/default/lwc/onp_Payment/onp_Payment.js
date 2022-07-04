834f2f682b3a58c551d7a8099ccd4da4a009ae45
takeshi.hirai <takeshi.hirai@oktana.com>
2022-07-04 16:16:02 -0500

    Payment Gateway

---
+++ force-app/main/default/lwc/onp_Payment/onp_Payment.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class ONP_Payment extends LightningElement {
+
+    handleButtonClick() {
+        alert('onp Payment request sent');
+    }
+
+}
